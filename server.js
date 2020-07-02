8a91b19dd80a92499c9378d00eb13a0e3295eaff
Jason Coleman <jayemcee121@gmail.com>
2020-07-02 19:00:41 -0400

    should hopefully fix

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ var app = express();
 
 var PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
 require("./routes/apiRoutes")(app);
