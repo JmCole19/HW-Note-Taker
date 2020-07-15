1441815e74b005579f4c46ad460f790a1f7c21b5
Jason Coleman <jayemcee121@gmail.com>
2020-07-14 21:57:43 -0400

    fixed i swear

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 var express = require("express");
-var apiRoutes = require("./routes/apiRoutes");
-var htmlRoutes = require("./routes/htmlRoutes");
 
 var app = express();
 
@@ -9,8 +7,6 @@ var PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.use("/api", apiRoutes);
-app.use("/", htmlRoutes);
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
